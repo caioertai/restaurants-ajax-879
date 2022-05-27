75a1fc085fbca6a3dac724a6ddb4eee438cfae03
Caio Andrade <andrade@hey.com>
2022-05-27 09:07:32 -0500

    Updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,22 @@
 puts 'Creating restaurants...'
-Restaurant.create!({
+restaurant1 = Restaurant.create!({
   name: "Le Dindon en Laisse",
   address: "18 Rue Beautreillis, 75004 Paris, France"
 })
-Restaurant.create!({
+restaurant2 = Restaurant.create!({
   name: "Neuf et Voisins",
   address: "Van Arteveldestraat 1, 1000 Brussels, Belgium"
 })
 puts 'Finished!'
 
+puts 'Creating reviews...'
+Review.create!(
+  restaurant: restaurant1,
+  content: "Great place!"
+)
+
+Review.create!(
+  restaurant: restaurant1,
+  content: "The food felt a bit off!"
+)
+puts 'Finished!'
